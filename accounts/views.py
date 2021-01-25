03427afbc10a9e4fc4aeaf4daf9808e589841ad8
Andrew Wolstenholme <awolste@clemson.edu>
2021-01-24 20:11:46 -0500

    Starter Project

---
+++ accounts/views.py
@@ -0,0 +1,28 @@
+"""
+This module contains our Django views for the "accounts" application.
+"""
+from django.shortcuts import render, redirect
+
+
+def login(request):
+    """function login This function handles the view for the login page of the application.
+
+    Args:
+        request (HTTPRequest): A http request object created automatically by Django.
+
+    Returns:
+        HttpResponse: A generated http response object to the request.
+    """
+    return render(request, "accounts/login.html")
+
+
+def logout(request):
+    """function logout This function handles the view for the logout page of the application.
+
+    Args:
+        request (HTTPRequest): A http request object created automatically by Django.
+
+    Returns:
+        HttpResponse: A generated http response object to the request.
+    """
+    return redirect("/")
