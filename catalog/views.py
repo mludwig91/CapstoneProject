3f120a4334dfd99cb79cedf6d1e84cc4ffd57eca
Robert C Haas <rchaas@clemson.edu>
2021-02-11 13:04:52 -0500

    whoops things should be working now

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+
+def shop(request):
+
+    return render(request, "catalog/shop.html")
+
+'''
+def shop(request):
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+    response = requests.get('https://openapi.etsy.com/v2/listings/active?api_key=/%s' % ip_address)
+    listings = response.json()
+'''
