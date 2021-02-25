c4e4eca68db17d7740104bc56d426333206c64fb
Andrew Wolstenholme <awolste@clemson.edu>
2021-02-25 16:52:30 -0500

    adding to sponsor app form

--- accounts/models.py
+++ accounts/models.py
@@ -10,12 +10,13 @@ from django.db.models.deletion import CASCADE, SET_NULL
 from django.db.models.fields import DateTimeField
 import datetime
 
+
 class SponsorCompany(models.Model):
     """
     Model of a sponsor company to keep track of info for sponsors' companies.
     """
     company_name = models.CharField("Company Name", max_length=25, validators=[MinLengthValidator(1)])
-    company_phone_number =  models.IntegerField("Phone Number", validators=[MinValueValidator(1000000000), MaxValueValidator(99999999999999)])
+    company_phone_number = models.IntegerField("Phone Number", validators=[MinValueValidator(1000000000), MaxValueValidator(99999999999999)])
     company_street_address = models.CharField("Company Street Address", max_length=32, validators=[MinLengthValidator(1)])
     company_city = models.CharField("Company City", max_length=25, validators=[MinLengthValidator(1)])
     company_state = models.CharField("Company State", max_length=25, validators=[MinLengthValidator(1)])
@@ -32,6 +33,7 @@ class SponsorCompany(models.Model):
         """
         return self.company_name
 
+
 class UserInformation(models.Model):
     """
     Contains a model of a user to keep track of user information.
@@ -39,11 +41,24 @@ class UserInformation(models.Model):
     # All of the fields in the model has validators to make sure they are valid.
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # Foreign key referring to an entry in the user table
 
-    #Two-tuple for roles where item 1 is the field value, and item 2 is the display name
+    # Two-tuple for roles where item 1 is the field value, and item 2 is the display name
     ROLE_NAME_CHOICES = (
         ('driver', 'Driver'),
         ('sponsor', 'Sponsor'),
         ('admin', 'Admin')
+    )
+
+    STATES = (
+        ('AL', 'Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkansas'), ('CA', 'California'),
+        ('CO', 'Colorado'), ('CT', 'Connecticut'), ('DE', 'Delaware'), ('DC', 'District of Columbia'),
+        ('FL', 'Florida'), ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'), ('IN', 'Indiana'),
+        ('IA', 'Iowa'), ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'),
+        ('MA', 'Massachusetts'), ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'), ('MO', 'Missouri'),
+        ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'), ('NH', 'New Hampshire'), ('NJ', 'New Jersey'),
+        ('NM', 'New Mexico'), ('NY', 'New York'), ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('OH', 'Ohio'),
+        ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'),
+        ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VT', 'Vermont'),
+        ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'), ('WI', 'Wisconsin'), ('WY', 'Wyoming')
     )
 
     role_name = models.CharField("Role Name", max_length=25, choices=ROLE_NAME_CHOICES, default="driver", validators=[MinLengthValidator(1)])
@@ -56,9 +71,9 @@ class UserInformation(models.Model):
     is_active_account = models.BooleanField("If User Has Account Enabled", default=True)
     sponsor_company = models.ForeignKey(SponsorCompany, on_delete=models.CASCADE, null=True, blank=True)
     points = models.IntegerField("Points", null=True, default=0)
-    address = models.CharField("Address", max_length=100, default="N/A")
-
-
+    address = models.CharField("Address", max_length=100, default="N/A", blank=True)
+    license_number = models.CharField("License", max_length=20, default="N/A", blank=True)
+    state = models.CharField("State", choices=STATES, max_length=20, blank=True)
 
     def __str__(self):
         """function __str__ is used to create a string representation of this class
@@ -77,6 +92,7 @@ class UserInformation(models.Model):
         return self.user.email
     get_user_email.short_description = 'User Email'  # Renames column head
 
+
 class CatalogItem(models.Model):
     """
     Model of a particular catalog item.
@@ -86,8 +102,9 @@ class CatalogItem(models.Model):
     retail_price = models.FloatField("Retail Price (MSRP)", null=True, validators=[MinValueValidator(0.01)])
     is_available = models.BooleanField("Item is Available From Retail", default=False)
     last_update = models.DateTimeField("DateTime of Last Update to Item", default=datetime.datetime.utcnow)
-    #URL Validator necessary for API entry?
+    # URL Validator necessary for API entry?
     api_item_Id = models.CharField("API Link/Identifier", max_length=256, validators=[MinLengthValidator(1)], unique=True)
+
 
 class CatalogItemImage(models.Model):
     """
@@ -95,7 +112,8 @@ class CatalogItemImage(models.Model):
     """
     catalog_item = models.ForeignKey(CatalogItem, on_delete=CASCADE)
     image_link = models.URLField("Static Image Link")
-    #filename = models.ImageField("Unique Image Filename", UniqueConstraint)
+    # filename = models.ImageField("Unique Image Filename", UniqueConstraint)
+
 
 class SponsorCatalogItem(models.Model):
     """
@@ -107,11 +125,12 @@ class SponsorCatalogItem(models.Model):
     last_update = models.DateTimeField("DateTime of Last Update to Item", default=datetime.datetime.utcnow)
     is_available_to_drivers = models.BooleanField("Is Item Available For Driver Redemption", default=False)
 
+
 class Order(models.Model):
     """
     Model of an order a driver may create (whether pending, completed, or otherwise).
     """
-    #Two-tuple for roles where item 1 is the field value, and item 2 is the display name
+    # Two-tuple for roles where item 1 is the field value, and item 2 is the display name
     ORDER_STATUS_CHOICES = (
         ('inCart', 'In-Cart'),
         ('ordered', 'Ordered'),
@@ -129,6 +148,7 @@ class Order(models.Model):
     retail_at_order = models.FloatField("Retail Price (MSRP) at Order Time", null=True, validators=[MinValueValidator(0.01)])
     points_at_order = models.IntegerField("Driver Point Cost at Order Time", null=True, validators=[MinValueValidator(1)])
 
+
 class AuditApplication(models.Model):
     """
     Model of a particular application being audited (regardless of application status).
@@ -140,13 +160,14 @@ class AuditApplication(models.Model):
     )
 
     submission_time = models.DateTimeField("DateTime of Application Submission")
-    #Associated to company so that any sponsor employee of company may approve/reject
+    # Associated to company so that any sponsor employee of company may approve/reject
     sponsor_company = models.ForeignKey(SponsorCompany, on_delete=CASCADE)
-    #Approving/Rejecting Sponsor
+    # Approving/Rejecting Sponsor
     sponsor = models.ForeignKey(UserInformation, on_delete=SET_NULL, null=True, related_name="%(class)s_sponsor", blank=True)
     driver = models.ForeignKey(UserInformation, on_delete=SET_NULL, null=True, related_name="%(class)s_driver")
     apply_status = models.CharField("Application Status", max_length=25, choices=APPLICATION_STATUS_CHOICES)
     reject_reason = models.CharField("Rejection Reason", max_length=128, blank=True)
+
 
 class AuditPointChange(models.Model):
     """
@@ -156,6 +177,7 @@ class AuditPointChange(models.Model):
     driver = models.ForeignKey(UserInformation, on_delete=CASCADE)
     point_change = models.IntegerField("Point Change for Driver")
     change_reason = models.CharField("Point Change Reason", max_length=128)
+
 
 class AuditLoginAttempt(models.Model):
     """
